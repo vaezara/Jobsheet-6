500e8c95c3ff947c878bed7a7378866160cc1967
vaezara <novaelzrayya@gmail.com>
2023-10-12 11:49:29 +0700

    modifikasi tanpa operator logika

--- Pemilihan2Percobaan120.java
+++ Pemilihan2Percobaan120.java
@@ -9,11 +9,17 @@ public class Pemilihan2Percobaan120 {
         tahun = input20.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0 || (tahun % 400) == 0)
+            if ((tahun % 100) == 0) {
+                if ((tahun % 400) == 0) {
                 System.out.println("Tahun Kabisat");
-            else
-                System.out.println("Bukan Tahun Kabisat");
+
+                }else
+                    System.out.println("Bukan Tahun Kabisat");
+            }else
+                System.out.println("Tahun Kabisat");
         } else
             System.out.println("Bukan Tahun Kabisat"); 
+    
+
     }
 }
